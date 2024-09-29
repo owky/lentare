f19556af8d2147622657f6e45322c8cbe7ebfdae
yuhei.ohwaki <yuhei.ohwaki@optim.co.jp>
2024-09-29 20:55:12 +0900

    Save access token on Firestore Cloud.

--- src/functions/oauth.js
+++ src/functions/oauth.js
@@ -1,12 +1,12 @@
 exports.handler = async function(event, context) {
-  const {google} = require('googleapis');
-  const jwt = require('jsonwebtoken');
+  const { google } = require('googleapis')
+  const { Firestore } = require('@google-cloud/firestore')
+  const jwt = require('jsonwebtoken')
 
   const CLIENT_ID = process.env.OAUTH_CLIENT_ID
   const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET
   const REDIRECT_PATH = '/.netlify/functions/oauth'
   const SCOPES = 'openid https://www.googleapis.com/auth/youtube.readonly'
-
   const appUrl = event.rawUrl.split('/.netlify')[0]
 
   const oauth2Client = new google.auth.OAuth2(
@@ -15,11 +15,21 @@ exports.handler = async function(event, context) {
     appUrl + REDIRECT_PATH
   );
 
+  const firestore = new Firestore({
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT)
+  })
+
   if (!!event.queryStringParameters.code) {
     let { tokens } = await oauth2Client.getToken(event.queryStringParameters.code)
-    console.log(tokens.access_token)
-
     const id_token = jwt.decode(tokens.id_token, { complete: true});
+
+    const docRef = firestore.collection('users').doc(id_token.payload.sub)
+    docRef.update({
+      accessToken: tokens.access_token,
+      refreshToken: tokens.refresh_token,
+      tokenExpiresAt: tokens.expiry_date,
+    })
 
     return {
       statusCode: 302,
