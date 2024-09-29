ea19ce5b4b4ae1dc018e794e9ddfaca46c4f56c1
yuhei.ohwaki <yuhei.ohwaki@optim.co.jp>
2024-09-29 20:12:21 +0900

    Auth button is displayed only when user sub is not found.

---
+++ src/components/Auth.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn v-show="userNotFound" @click="auth">Google Authentication</v-btn>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const userNotFound = !(localStorage.getItem(APP_ID) && JSON.parse(localStorage.getItem(APP_ID)).sub)
+
+function auth() {
+  window.location.href = window.location.origin + "/.netlify/functions/oauth"
+}
+</script>
+
+<style scoped>
+</style>
