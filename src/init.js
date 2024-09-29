7f1b2bfdbd4256b1befd4caba3100ad476f19894
yuhei.ohwaki <yuhei.ohwaki@optim.co.jp>
2024-09-29 20:23:29 +0900

    Initialize login is moved to index.html

--- src/init.js
+++
@@ -1,8 +0,0 @@
-const APP_ID = "lentare"
-
-const sub = new URLSearchParams(window.location.search).get('sub')
-
-if (!!sub) {
-  localStorage.setItem(APP_ID, JSON.stringify({sub: sub}))
-  window.history.replaceState({}, '', '/')
-}
